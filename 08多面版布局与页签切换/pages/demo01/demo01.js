ec20b69cbb7c41dec8b670125297de059c74b8d2
study <study@qq.com>
2024-03-25 14:38:01 +0800

    test1

---
+++ 08多面版布局与页签切换/pages/demo01/demo01.js
@@ -0,0 +1,75 @@
+// pages/demo01/demo01.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        list:[
+        "手机数码",
+        "男装推荐",
+        "女装推荐",
+        "优选水果",
+        "家用电器",
+        "运动户外",
+        "电脑办公",
+        "体育用品",
+        "美妆护肤"]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
