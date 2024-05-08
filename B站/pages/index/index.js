8b5f5bcca7768cb181c216123246dbe1c4d1b519
DanTa <123435243+DanTa368@users.noreply.github.com>
2024-05-08 11:29:14 +0800

    Add files via upload

---
+++ B站/pages/index/index.js
@@ -0,0 +1,128 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //首页导航数据 声明变量 空数组
+    navList:[],
+    swiperList:[],
+    videosList:[],
+    currentIndexNav:0
+  },
+  //获取首页导航数据 getNavList 函数
+  getNavList(){
+    let that =this;
+    //利用小程序内置发送请求request方法 获取数据
+    wx.request({
+      url: 'https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/navList',
+      //请求成功后用回调函数 success  res参数表示返回值
+      success(res){
+        // console.log(res);
+        if(res.data.code===0){
+          that.setData({
+            navList:res.data.data.navList
+          })
+        }
+      }
+    })
+  },
+  getSwiperList(){
+    let that =this;
+    //利用小程序内置发送请求request方法 获取数据
+    wx.request({
+      url: 'https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/swiperList',
+      //请求成功后用回调函数 success  res参数表示返回值
+      success(res){
+        // console.log(res);
+        if(res.data.code===0){
+          that.setData({
+            swiperList:res.data.data.swiperList
+          })
+        }
+      }
+    })
+  },
+  getVideosList(){
+    let that =this;
+    //利用小程序内置发送请求request方法 获取数据
+    wx.request({
+      url: 'https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/videosList',
+      //请求成功后用回调函数 success  res参数表示返回值
+      success(res){
+        // console.log(res);
+        if(res.data.code===0){
+          that.setData({
+            videosList:res.data.data.videosList
+          })
+        }
+      }
+    })
+  },
+  activeNav(e){
+    // console.log(e.target.dataset.index)
+    this.setData({
+      currentIndexNav:e.target.dataset.index
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //1 获取首页数据导航
+    this.getNavList();
+    //2. 调用轮播图数据
+    this.getSwiperList();
+    //3 视频数据
+    this.getVideosList();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
