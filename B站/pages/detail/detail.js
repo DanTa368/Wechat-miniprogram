260dee31809960c9b683d4b016f309e56752ac4a
DanTa <123435243+DanTa368@users.noreply.github.com>
2024-05-29 12:06:37 +0800

    Add files via upload

--- B站/pages/detail/detail.js
+++ B站/pages/detail/detail.js
@@ -1,76 +1,158 @@
-import { request } from "../../request/index.js"
+// 0 引入 用来发送请求的 方法 一定要把路径补全
+import { request } from "../../request/index.js";
 Page({
 
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    //视频详情
+    videoInfo: null,
+    // 推荐视频
+    othersList: [],
+    // 评论数据
+    commentData: null
+
+  },
+  /**
+   * 根据视频的id获取视频详情
+   */
+  getCurrentVideo(videoId) {
+    // let that = this;
+    // wx.request({
+    //   url: 'https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/videoInfo?id=' + videoId,
+    //   success(res) {
+    //     //  console.log(res);
+    //      if (res.statusCode === 200) {
+    //         that.setData({
+    //             videoInfo: res.data.data.videoInfo
+    //            })
+    //         }
+    //     }
+    // })
+    request({ url: "https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/videoInfo?id="+videoId })
+    .then(result => {
+      this.setData({
+        videoInfo: result.data.data.videoInfo
+      })
+    })
+  },
+  /**
+   * 获取推荐视频
+   */
+  getOthersList(videoId) {
+    // let that = this;
+    // wx.request({
+    //   url: "https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/othersList?id=" + videoId,
+    //   success(res) {
+    //     console.log(res);
+    //     if (res.statusCode === 200) {
+    //       that.setData({
+    //         othersList: res.data.data.othersList
+    //       })
+    //     }
+    //   }
+    // })
+    request({ url: "https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/othersList?id="+videoId })
+    .then(result => {
+      this.setData({
+        othersList: result.data.data.othersList
+      })
+    })
+    // 关闭下拉刷新的窗口 如果没有调用下拉刷新的窗口 直接关闭也不会报错  
+      wx.stopPullDownRefresh();
+  },
     /**
-     * 页面的初始数据
-     */
-    data: {
-        videoInfo: [],
-    },
-    // 根据id查询视频信息
-    getCurrentVideoInfo(videoId) {
-        request({url:`https://mock.mengxuegu.com/mock/662f3242026ee44c07ff9eb9/bi/videoInfo?id=${videoId}`,})
-        .then(res =>{
-            this.setData({
-                videoInfo:res.data.data.videoInfo
-            })
+  * 获取评论数据
+  */
+ getCommentList(videoId) {
+    // let that = this;
+    // wx.request({
+    //   url: "https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/commentsList?id=" + videoId,
+    //   success(res) {
+    //     console.log(res);
+    //     if (res.statusCode === 200) {
+    //       that.setData({
+    //         commentData: res.data.data.commentData
+    //       })
+    //     }
+    //   }
+    // })
+    
+    request({ url: "https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/commentsList?id="+videoId })
+    .then(result => {
+      this.setData({
+        commentData: result.data.data.commentData
+      })
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //console.log(options);
+    let videoId = options.id;
+
+    this.getCurrentVideo(videoId);
+    this.getOthersList(videoId);
+    this.getCommentList(videoId);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    //  console.log('%c'+"刷新","color:red;font-size:100px;background-image:linear-gradient(to right,#0094ff,pink)");
+      wx.showLoading({
+          title: '加载中',
         })
+         setTimeout(function() {
+          wx.hideLoading()
+        }, 3000);
+  },
 
-    },
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
-        let videoId = options.id;
-        this.getCurrentVideoInfo(videoId);
-    },
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    //   console.log('页面触底');
+    wx.showToast({ title: '没有下一页数据' });
+  },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
 
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
+  }
 })
--- B站/pages/index/index.js
+++ B站/pages/index/index.js
@@ -1,149 +1,184 @@
-// 引入请求方法
-import {request} from "../../request/index.js";
-
+// 0 引入 用来发送请求的 方法 一定要把路径补全
+import { request } from "../../request/index.js";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    // 被点击的首页导航的菜单索引
-    currentIndexNav:0,
+    // 被点击的首页导航的菜单的索引
+    currentIndexNav: 0,
     //首页导航数据 声明变量 空数组
-    navList:[],
-    //轮播图数组
-    swiperList:[],
-    //视频列表数据
-    videosList:[]
+    navList: [],
+    // 轮播图数据
+    swiperList: [],
+    // 视频列表数据
+    videosList: [],
+    swiperHeight:2225,
+    arr:['.v1','.v2','.v3']
   },
-  //获取首页导航数据 getNavList 函数
-  getNavList(){
-    let that =this;
-    //利用小程序内置发送请求request方法 获取数据
-    wx.request({
-      url: 'https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/navList',
-      //请求成功后用回调函数 success  res参数表示返回值
-      success(res){
-        // console.log(res);
-        if(res.data.code===0){
-          that.setData({
-            navList:res.data.data.navList
-          })
-        }
-      }
+
+  // 点击首页导航按钮
+  activeNav(e) {
+    //console.log(123);
+    this.setData({
+      currentIndexNav: e.target.dataset.index
     })
   },
-  //点击首页导航按钮
-  activeNav(e){
-    //  console.log(e.target.dataset.index);
-     this.setData({
-       currentIndexNav:e.target.dataset.index
-     })
-  },
-   //获取首页轮播图数据 getSwiperList 函数
-   getSwiperList(){
-    // let that =this;
-    // 利用小程序内置发送请求request方法 获取数据
+  /**
+   *获取首页导航数据 getNavList 函数
+   */
+  getNavList() {
+    // let that = this;
+    // //利用小程序内置发送请求的request方法 获取数据
     // wx.request({
-    //   url: 'https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/swiperList',
-    //   //请求成功后用回调函数 success  res参数表示返回值
-    //   success(res){
+    //   url: "https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/navList",
+    //   //请求成功后启用回调函数success res 参数表示返回值
+    //   success(res) {
     //     // console.log(res);
-    //     if(res.data.code===0){
+    //     if (res.data.code === 0) {
     //       that.setData({
-    //         swiperList:res.data.data.swiperList
+    //         navList: res.data.data.navList
     //       })
     //     }
     //   }
     // })
-    request({url:"https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/swiperList"})
-    .then(result =>{
+    request({ url: "https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/navList" })
+    .then(result => {
       this.setData({
-        swiperList:result.data.data.swiperList
+        navList: result.data.data.navList
       })
     })
   },
-    //获取首页视频列表数据 getVideosList 函数
-    getVideosList(){
-      // let that =this;
-      //利用小程序内置发送请求request方法 获取数据
-      // wx.request({
-      //   url: 'https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/videosList',
-      //   //请求成功后用回调函数 success  res参数表示返回值
-      //   success(res){
-      //     // console.log(res);
-      //     if(res.data.code===0){
-      //       that.setData({
-      //         videosList:res.data.data.videosList
-      //       })
-      //     }
-      //   }
-      // })
-      request({url:'https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/videosList'})
-      .then(result =>{
-        this.setData({
-          videosList:result.data.data.videosList
-        })
+  /**
+   * 获取轮播图数据
+   */
+  getSwiperList() {
+    // let that = this;
+    // wx.request({
+    //   url: "https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/swiperList",
+    //   success(res) {
+    //     // console.log(res);
+    //     if (res.data.code === 0) {
+    //       that.setData({
+    //         swiperList: res.data.data.swiperList
+    //       })
+    //     }
+    //   }
+    // })
+    request({ url: "https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/swiperList" })
+    .then(result => {
+      this.setData({
+        swiperList: result.data.data.swiperList
       })
-    },
+    })
+  },
+  /**
+  * 获取视频列表
+  */
+  getVideosList() {
+    // let that = this;
+    // wx.request({
+    //   url: "https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/videosList",
+    //   success(res) {
+    //     // console.log(res);
+    //     if (res.data.code === 0) {
+    //       that.setData({
+    //         videosList: res.data.data.videosList
+    //       })
+    //     }
+    //   }
+    // })
+    request({ url: "https://mock.mengxuegu.com/mock/66225d53faa39b45675966ef/bi/videosList" })
+    .then(result => {
+      this.setData({
+        videosList: result.data.data.videosList
+      })
+    })
+  },
+  //赋值高度
+  setSwiperHeight(v) {
+    let query = wx.createSelectorQuery().in(this);
+    query.select(v).boundingClientRect();
+    query.exec((res)=>{
+        let headerHeight = res[0].height;
+        console.log(headerHeight);
+         this.setData({
+          swiperHeight: headerHeight
+        }); 
+    })
+  },
+
+  //滑动切换
+  bindChange(e){
+    var that =this;
+    that.setData({
+        currentIndexNav:e.detail.current
+    });
+    this.setSwiperHeight(this.data.arr[e.detail.current]);
+  },
+  //下拉滚动
+  onPageScroll(){
+    this.setSwiperHeight(this.data.arr[this.data.currentIndexNav]);
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    //1 获取首页数据导航
+    //1 获取首页导航数据
     this.getNavList();
-    //2调用获取轮播图数据的函数
+    // 2 调用获取轮播图数据的函数
     this.getSwiperList();
-    //3调用获取视频列表数据的函数‘
-    this. getVideosList();
+    // 3 调用获取视频列表函数
+    this.getVideosList();
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
